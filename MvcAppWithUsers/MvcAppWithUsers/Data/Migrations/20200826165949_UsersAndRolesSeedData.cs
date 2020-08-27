0c6792c3d9bcc0e9e9553bf08bbbec5a1b641a41
leontin.murani <leontin.murani@gmail.com>
2020-08-27 14:43:34 +0300

    Add users and roles seed data. Add role based Authentication.

---
+++ MvcAppWithUsers/MvcAppWithUsers/Data/Migrations/20200826165949_UsersAndRolesSeedData.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MvcAppWithUsers.Data.Migrations
+{
+    public partial class UsersAndRolesSeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddRole("Admin");
+            migrationBuilder.AddRole("BackOfficeUser");
+            migrationBuilder.AddRole("FrontOfficeUser");
+
+            migrationBuilder.AddUserWithRoles(
+               email: "admin@fasttrackit.ro",
+               password: "Admin123!",
+               new[] { "Admin", "BackOfficeUser", "FrontOfficeUser" });
+
+            migrationBuilder.AddUserWithRoles(
+                "back-office@fasttrackit.ro",
+                "Backoffice123!",
+                new[] { "BackOfficeUser" });
+
+            migrationBuilder.AddUserWithRoles(
+                "front-office@fasttrackit.ro",
+                "Frontoffice123!",
+                new[] { "FrontOfficeUser" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
